fc123a07954ce7249a7ea99f9bd9bcce36bf2189
Eugene <lzhnek@gmail.com>
2017-09-11 08:04:12 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
 	var myName = prompt ('Please enter your name');
 	localStorage.setItem('name', myName);
-	myHeading.innerHTML = myHeading.innerHTML + myName ;
+	myHeading.innerHTML = 'Добро пожаловать на мою тестовую страничку ' + myName ;
 }
 
 if (!localStorage.getItem('name')) {
