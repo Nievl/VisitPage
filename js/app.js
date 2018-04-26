627963f155edcf5cac510257b1dbe48fc9568286
Eugene <lzhnek@gmail.com>
2018-04-26 10:48:52 +0300

    Configure webpack, add CSS loader. Add Botstrap styles

--- js/app.js
+++ js/app.js
@@ -1,33 +1,56 @@
-import articles from './fixtures.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import articles from "./fixtures.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const jsx = <img src="./js/react/react.svg" className="subtitle" />;
 
 class App extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     isOpen: true
+  //   };
+  // }
   render() {
     const articleList = articles.map(item => {
       return (
-      <div key={item.id} className="article">
-          <div className="title">{item.title}</div>
-          <div className="date">Creation date:{item.date.toString()}</div>
-          <div className="text">
+        <div key={item.id} className="card-body">
+          <div className="card-header">
+            <h2>
+              {item.title}
+              {/* <button onClick={this.handleClick} className="btn btn-primary bt-lg">
+                {this.state.isOpen ? "close" : "open"}                
+              </button> */}
+            </h2>
+          </div>
+          <div className="card-subtitle text-muted">
+            Creation date:{item.date.toString()}
+          </div>
+          <div className="card-text">
+            {/* {this.state.isOpen && item.text} */}
             {item.text}
             <form className="link" action={item.link}>
-              <button type="submit">link</button>
+              <button className="btn btn-link btn-lg" type="submit">
+                link
+              </button>
             </form>
           </div>
-      </div>
+        </div>
       );
     });
     return (
-      <div>
-        <div className="articleList">{articleList}</div>
+      <div className="container">
+        <div className="">{articleList}</div>
         <footer>{jsx}</footer>
       </div>
     );
   }
 }
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+// function handleClick() {
+//   this.state
+//   console.log("click");
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  entry: './js/app.js',
+  entry: "./js/app.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'js')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      loader: 'babel-loader',
-      query: { 
-        cacheDirectory: true, // включить кэширование
-        presets: ['es2015', 'react']
+    rules: [
+      {
+        test: /.jsx?$/,
+        include: [path.resolve(__dirname, "js")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader",
+        query: {
+          cacheDirectory: true, // включить кэширование
+          presets: ["es2015", "react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
-    }]
+    ]
   },
   resolve: {
-    extensions: ['.json', '.js', '.jsx', '.css']
+    extensions: [".json", ".js", ".jsx", ".css"]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
-    publicPath: path.join('/dist/')
+    publicPath: path.join("/dist/")
   }
 };
